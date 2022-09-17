513fb21cbf22348bbe0c71a25d6eb47417743dde
DSDmark <sahup3296@gmail.com>
2022-09-17 09:35:57 +0530

    🏁 FINAL COMMIT

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# bouncingElement
-Created with CodeSandbox
+<div align="center">
+
+# BouncingBall
+
+<div>
+
+---
+
+<div align="center">
+
+<div align="left">
+
+## Purpose
+
+> How Things Works in Games 
+
+---
+
+### How to play
+
+1. Click on green **Clone or download** button and choose Download ZIP.
+2. Find the downloaded zipped file on your pc and extract it.
+3. Go to **Web-Info** folder. Inside you will find a file named **index.html**.
+4. Double click on **index.html** file and your browser will open (or maybe just a new tab) and the game will start.
+5. If for whatever reason your browser does not open, right click on **index.html** file go down to **Open with**
+   and choose your web browser. This will open the browser and launch the game.
+
+Once you are done playing you can close the tab in your browser to quit the game.
+
+---
+
+</div>
+
+### BouncingBall
+
+![BouncingBall preview](assets/images/preview.gif "BouncingBall")
+
+</div>
+
+> If you have any issues with the game feel free to let me know!
+
+> If you are more interested, check out the collection of others [ **DSDmark**](https://github.com/DSDmark/ "DSDmark").
+
+---
---
+++ assets/js/index.js
@@ -0,0 +1,80 @@
+const canvas = document.getElementById("cvs");
+const ctx = canvas.getContext("2d");
+const pattern = document.getElementById("pattern");
+
+const CH = (canvas.height = 400);
+const CW = (canvas.width = 400);
+
+canvas.style.borderLeft = "5px solid black";
+canvas.style.borderRight = "5px solid black";
+canvas.style.borderTop = "5px solid white";
+canvas.style.borderBottom = "5px solid white";
+
+let showBallPath = false;
+
+function showPath() {
+  showBallPath = !showBallPath;
+}
+
+pattern.addEventListener("onclick", showPath);
+
+const bounceBall = {
+  speed: 5,
+  velocity_x: 2,
+  velocity_y: 2,
+  radius: 10,
+  color: "white",
+  angle: -30, //! BY DEFAULT COMPUTER CONSIDER ANGLE IN RADIUS . THATAWAY -30.
+  position: {
+    x: CW / 2,
+    y: CH / 2,
+  },
+
+  draw() {
+    showBallPath ? "" : ctx.clearRect(0, 0, CH, CW); // ?FOR CLEAR PERVIOUS CANVAS
+    ctx.fillStyle = this.color;
+    ctx.beginPath();
+    ctx.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI);
+    ctx.closePath();
+    ctx.fill();
+  },
+  update() {
+    if (
+      this.position.x + this.radius > CW ||
+      this.position.x - this.radius < 0
+    ) {
+      this.angle = 180 - this.angle;
+      this.color = "black";
+    }
+    if (
+      this.position.y + this.radius > CH ||
+      this.position.y - this.radius < 0
+    ) {
+      this.angle = 360 - this.angle;
+      this.color = "white";
+    }
+
+    this.velocity_x = Math.cos(this.angle * (Math.PI / 180)) * this.speed;
+    this.velocity_y = Math.sin(this.angle * (Math.PI / 180)) * this.speed;
+    this.position.x += this.velocity_x;
+    this.position.y += this.velocity_y;
+  },
+};
+
+function render() {
+  bounceBall.draw();
+  bounceBall.update();
+}
+
+function gameLoop() {
+  render();
+
+  window.requestAnimationFrame(gameLoop);
+}
+window.requestAnimationFrame(gameLoop);
+
+//? YOUR CAN USE SETINTERVAL FUNCTION AS WELL BUT REQUESTANIMATIONFRAME HAS OWN ADVANTAGES
+
+// setInterval(() => {
+//   gameLoop();
+// }, 1000 / 60);
